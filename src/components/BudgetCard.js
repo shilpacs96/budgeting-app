0c7a42af6b55f2b4d101aa2730905537c062e497
shilpacs96 <shilpa.cs96@gmail.com>
2024-03-18 15:11:48 +0530

    first commit

---
+++ src/components/BudgetCard.js
@@ -0,0 +1,62 @@
+import React from "react";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardTitle,
+  ProgressBar,
+  Stack,
+} from "react-bootstrap";
+import { currencyFormatter } from "../utils";
+
+function BudgetCard({ name, amount, max, gray }) {
+  const classNames = [];
+  if (amount > max) {
+    classNames.push("bg-danger", "bg-opacity-10");
+  } else if (gray) {
+    classNames.push("bg-light");
+  }
+  return (
+    <div>
+      <Card className={classNames.join(" ")}>
+        <CardBody>
+          <CardTitle className="d-flex justify-content-between align-items-baseline fw-normal mb-3">
+            <div className="me-2">{name}</div>
+            <div className="d-flex align-items-baseline">
+              {currencyFormatter.format(amount)}
+              <span className="text-muted fs-6 ms-1">
+                /{currencyFormatter.format(max)}
+              </span>
+            </div>
+          </CardTitle>
+          <ProgressBar
+            className="rounded-pill"
+            variant={getProgressBarVariant(amount, max)}
+            min={0}
+            max={max}
+            now={amount}
+          />
+          <Stack direction="horizontal" gap={2} className="mt-4">
+            <Button variant="outline-primary" className="ms-auto">
+              Add Expense
+            </Button>
+            <Button variant="outline-secondary">View Expense</Button>
+          </Stack>
+        </CardBody>
+      </Card>
+    </div>
+  );
+}
+
+export default BudgetCard;
+
+function getProgressBarVariant(amount, max) {
+  const ratio = amount / max;
+  if (ratio < 0.5) {
+    return "primary";
+  }
+  if (ratio < 0.75) {
+    return "warning";
+  }
+  return "danger";
+}
